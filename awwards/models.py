50025ca2eb565cccc7c9e39c7e3e206cb40785b3
Cecilia Barasa <cecibarasa@gmail.com>
2020-07-09 11:18:24 +0300

    add proect model

--- awwards/models.py
+++ awwards/models.py
@@ -4,5 +4,22 @@ from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     profile_picture = CloudinaryField('images')
-    bio = models.TextField
-    contact = models.CharField(max_length = 100, blank=True)
+    bio = models.TextField()
+    contact = models.CharField(max_length=100, blank=True)
+    
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    link = models.CharField(max_length=150)
+    image = CloudinaryField('images')
+
+    def __str__(self):
+        return self.title          
