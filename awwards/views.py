1fc046902927aa50079f490d58a021dbda6c6ecd
Cecilia Barasa <cecibarasa@gmail.com>
2020-07-09 08:57:33 +0300

    add date function

--- awwards/views.py
+++ awwards/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 def index(request):
     return HttpResponse('Awwards')
+
+def awward_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1>Awward for {day} {date.day}-{date.month}--{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)            
