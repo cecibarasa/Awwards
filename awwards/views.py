0204fb464e4898154d7208be33bb2e43a805bf1d
Cecilia Barasa <cecibarasa@gmail.com>
2020-07-09 10:35:51 +0300

    display navbar

--- awwards/views.py
+++ awwards/views.py
@@ -4,15 +4,4 @@ import datetime as dt
 
 def index(request):
     date = dt.date.today()
-    return render(request, 'index.html', {"date":date})
-
-def awward_of_day(request):
-    date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1>Awward for {day} {date.day}-{date.month}--{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)            
+    return render(request, 'index.html', {"date":date})     
